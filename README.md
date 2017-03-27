46bd6c71b29efd0820353fb6dde8c7eb7cfc53b8
David Wu <wudayu@gmail.com>
2017-03-27 16:54:50 +0800

    update 4up settings

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # homelink
+
+###Test
+
+		scrapy shell "http://nj.lianjia.com/ershoufang/y2/"
+
+###Run
+
+		scrapy crawl hl
+
+###Change
+
+		file hl_spider line 23
+		change pages
+
+###Notice
+
+		120 pages per ipaddress
--- homelink/spiders/hl_spider.py
+++ homelink/spiders/hl_spider.py
@@ -3,7 +3,7 @@ from homelink.items import HomelinkItem
 import csv
 
 class HlSpider(scrapy.Spider):
-    base_url = "http://nj.lianjia.com/ershoufang/lc2f2y2"
+    base_url = "http://nj.lianjia.com/ershoufang/y3f2f5sf1l4l5/"
     url_suffix = "/"
     init_url = base_url + url_suffix
 
@@ -13,16 +13,18 @@ class HlSpider(scrapy.Spider):
         init_url
     ]
 
+    csvfile = file('csv_test.csv', 'wb')
+    writer = csv.writer(csvfile)
+
     def parse(self, response):
         page_count_unicode = response.xpath('//div[@class="page-box house-lst-page-box"]//@page-data').extract_first()
         page_count = int(filter(str.isdigit, page_count_unicode[:18].encode('utf-8')))
 
         for curr_page in range(1, page_count + 1):
-            print '\n'
             print str(curr_page)
-            print '\n'
             yield scrapy.Request(response.urljoin(self.base_url + "pg" + str(curr_page) + self.url_suffix), self.parse_list_page)
-            print '\n'
+
+        #csvfile.close()
 
     def parse_list_page(self, response):
         links = []
@@ -33,22 +35,23 @@ class HlSpider(scrapy.Spider):
 
     def parse_inner_page(self, response):
         item = HomelinkItem()
-        item['communityName'] = response.xpath('//div[@class="communityName"]/a[@class="info"]/text()').extract_first()
+        item['communityName'] = response.xpath('//div[@class="communityName"]/a[@class="info"]/text()').extract_first().encode('utf-8')
         item['url'] = response.url
-        item['title'] = response.xpath('//h1[@class="main"]/text()').extract_first()
-        item['houseType'] = response.xpath('//div[@class="room"]/div[@class="mainInfo"]/text()').extract_first()
+        item['title'] = response.xpath('//h1[@class="main"]/text()').extract_first().encode('utf-8')
+        item['houseType'] = response.xpath('//div[@class="room"]/div[@class="mainInfo"]/text()').extract_first().encode('utf-8')
         item['unitPrice'] = int(response.xpath('//span[@class="unitPriceValue"]/text()').extract_first())
         item['totalPrice'] = int(response.xpath('//span[@class="total"]/text()').extract_first())
         item['area'] = float(response.xpath('//div[@class="area"]/div[@class="mainInfo"]/text()').extract_first().encode('utf-8')[:-6])
-        item['floor'] = response.xpath('//div[@class="room"]/div[@class="subInfo"]/text()').extract_first()
-        item['heading'] = response.xpath('//div[@class="type"]/div[@class="mainInfo"]/text()').extract_first()
-        item['decoration'] = response.xpath('//div[@class="type"]/div[@class="subInfo"]/text()').extract_first()
+        item['floor'] = response.xpath('//div[@class="room"]/div[@class="subInfo"]/text()').extract_first().encode('utf-8')
+        item['heading'] = response.xpath('//div[@class="type"]/div[@class="mainInfo"]/text()').extract_first().encode('utf-8')
+        item['decoration'] = response.xpath('//div[@class="type"]/div[@class="subInfo"]/text()').extract_first().encode('utf-8')
         item['builtYear'] = int(filter(str.isalnum, response.xpath('//div[@class="area"]/div[@class="subInfo"]/text()').extract_first().encode('utf-8')))
         item['schoolNames'] = response.xpath('//div[@class="schoolName"]/span[@class="info"]/a/text()').extract()
-        item['district'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[0]
-        item['token'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[1]
-        item['metro'] = response.xpath('//div[@class="areaName"]/a/text()').extract_first()
+        item['district'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[0].encode('utf-8')
+        item['token'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[1].encode('utf-8')
+        item['metro'] = response.xpath('//div[@class="areaName"]/a/text()').extract_first(default='N/A').encode('utf-8')
 
+        '''
         print "CommunityName : " + item['communityName']
         print "Url : " + item['url']
         print "Title : " + item['title']
@@ -68,3 +71,9 @@ class HlSpider(scrapy.Spider):
             print "Metro : " + item['metro']
 
         print '\n'
+        '''
+        schoolNames = ""
+        for schoolName in item['schoolNames']:
+            schoolNames = schoolNames + schoolName.encode('utf-8') + ' '
+
+        self.writer.writerow([item['communityName'], item['url'], item['title'], item['houseType'], str(item['unitPrice']), str(item['totalPrice']), str(item['area']), item['floor'], item['heading'], item['decoration'], str(item['builtYear']), schoolNames, item['district'], item['token'], item['metro']])
