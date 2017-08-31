4bfce654db40c4cc8a4e24881a9238d71ea4effc
David Wu <wudayu@gmail.com>
2017-08-31 14:30:18 +0800

    modify some

--- homelink/pipelines.py
+++ homelink/pipelines.py
@@ -5,7 +5,42 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy import signals
+from scrapy.exporters import CsvItemExporter
 
 class HomelinkPipeline(object):
     def process_item(self, item, spider):
+
+
+        self.writer.writerow([item['communityName'], item['url'], item['title'], item['houseType'], str(item['unitPrice']), str(item['totalPrice']), str(item['area']), item['floor'], item['heading'], item['decoration'], str(item['builtYear']), schoolNames, item['district'], item['token'], item['metro']])
+
+        return item
+
+class CsvPipeline(object):
+    def __init__(self):
+        self.files = {}
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipeline = cls()
+        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+        return pipeline
+
+    def spider_opened(self, spider):
+        file = open('%s_items.csv' % spider.name, 'w+b')
+        self.files[spider] = file
+        self.exporter = CsvItemExporter(file)
+        self.exporter.fields_to_export = ['communityName', 'url', 'title', 'houseType', 'unitPrice', 'totalPrice', 'area', 'floor', 'heading', 'decoration', 'builtYear''district', 'token', 'metro']
+        # self.exporter.fields_to_export = ['communityName', 'url', 'title', 'houseType', 'unitPrice', 'totalPrice', 'area', 'floor', 'heading', 'decoration', 'builtYear''district', 'token', 'metro']
+
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
--- homelink/spiders/hl_spider.py
+++ homelink/spiders/hl_spider.py
@@ -1,6 +1,5 @@
 import scrapy
 from homelink.items import HomelinkItem
-import csv
 
 class HlSpider(scrapy.Spider):
     base_url = "https://nj.lianjia.com/ershoufang/"
@@ -12,9 +11,6 @@ class HlSpider(scrapy.Spider):
     start_urls = [
         init_url
     ]
-
-    csvfile = file('csv_test.csv', 'wb')
-    writer = csv.writer(csvfile)
 
     def parse(self, response):
         page_count_unicode = response.xpath('//div[@class="page-box house-lst-page-box"]//@page-data').extract_first()
@@ -46,34 +42,13 @@ class HlSpider(scrapy.Spider):
         item['heading'] = response.xpath('//div[@class="type"]/div[@class="mainInfo"]/text()').extract_first().encode('utf-8')
         item['decoration'] = response.xpath('//div[@class="type"]/div[@class="subInfo"]/text()').extract_first().encode('utf-8')
         item['builtYear'] = int(filter(str.isalnum, response.xpath('//div[@class="area"]/div[@class="subInfo"]/text()').extract_first().encode('utf-8')))
-        item['schoolNames'] = response.xpath('//div[@class="schoolName"]/span[@class="info"]/a/text()').extract()
+        schoolNames = response.xpath('//div[@class="schoolName"]/span[@class="info"]/a/text()').extract()
         item['district'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[0].encode('utf-8')
         item['token'] = response.xpath('//div[@class="areaName"]/span[@class="info"]/a/text()').extract()[1].encode('utf-8')
         item['metro'] = response.xpath('//div[@class="areaName"]/a/text()').extract_first(default='N/A').encode('utf-8')
 
-        '''
-        print "CommunityName : " + item['communityName']
-        print "Url : " + item['url']
-        print "Title : " + item['title']
-        print "HouseType : " + item['houseType']
-        print "UnitPrice : " + str(item['unitPrice'])
-        print "TotalPrice : " + str(item['totalPrice']) + "w"
-        print "Area : " + str(item['area']) + "m2"
-        print "Floor : " + item['floor']
-        print "Heading : " + item['heading']
-        print "Decoration : " + item['decoration']
-        print "BuiltYear : " + str(item['builtYear'])
-        for schoolName in item['schoolNames']:
-            print "School : " + schoolName
-        print "District : " + item['district']
-        print "Token : " + item['token']
-        if item['metro']:
-            print "Metro : " + item['metro']
+        item['schoolNames'] = ''
+        for schoolName in schoolNames:
+            item['schoolNames'] = schoolNames + schoolName.encode('utf-8') + ' '
 
-        print '\n'
-        '''
-        schoolNames = ""
-        for schoolName in item['schoolNames']:
-            schoolNames = schoolNames + schoolName.encode('utf-8') + ' '
-
-        self.writer.writerow([item['communityName'], item['url'], item['title'], item['houseType'], str(item['unitPrice']), str(item['totalPrice']), str(item['area']), item['floor'], item['heading'], item['decoration'], str(item['builtYear']), schoolNames, item['district'], item['token'], item['metro']])
+        yield item
